0789d5ffdc6e3337888cc772ccaddcd1aef9bed4
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-03-27 20:51:20 -0300

    Add luxon

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import BookList from "./modules/BookList.js";
 import navigation from './modules/navigation.js';
-import displayDate from './modules/displayDate.js';
+import { DateTime } from 'luxon.js';
+
+function displayDate() {
+  const currentDate = DateTime.local().toLocaleString(DateTime.DATE_FULL);
+  const dateEl = document.getElementById('date');
+  dateEl.textContent = currentDate;
+}
 
 window.addEventListener('load', displayDate);
+
 
 const bookList = new BookList();
