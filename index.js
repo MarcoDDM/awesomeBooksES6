2d0fe9a5446ec6e62a199836a03e6414bb7ecf50
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-03-28 12:48:20 -0300

    Add semi colon

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const displayDate = () => {
   const currentDate = DateTime.local().toLocaleString(DateTime.DATE_FULL);
   const dateEl = document.getElementById('date');
   dateEl.textContent = currentDate;
-}
+};
 
 window.addEventListener('load', displayDate);
 
