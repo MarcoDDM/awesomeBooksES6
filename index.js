007a4e8c19bb919bc314ef187c2437e17ca57f33
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-03-28 12:44:10 -0300

    Change displayDate to arrow function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { DateTime } from './node_modules/luxon/src/luxon.js';
 import BookList from './modules/BookList.js';
 import navigation from './modules/navigation.js';
 
-function displayDate() {
+const displayDate = () => {
   const currentDate = DateTime.local().toLocaleString(DateTime.DATE_FULL);
   const dateEl = document.getElementById('date');
   dateEl.textContent = currentDate;
