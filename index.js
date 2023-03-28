3d17f82cf1c5259b901baddbf46d86630ed9ae53
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-03-28 12:40:21 -0300

    Change on luxon path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { DateTime } from 'luxon/src/luxon.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 import BookList from './modules/BookList.js';
 import navigation from './modules/navigation.js';
 
